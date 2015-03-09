5fa08a9e6d14ad2f10b7ee699ac3f38782e35fa5
Crazcalm <crazcalm@gmail.com>
2015-03-09 01:30:49 -0400

    Finalizing the code

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,10 +1,18 @@
 import csv
 from collections import namedtuple
 from constants import SAT_CSV
-from operator import itemgetter, attrgetter
+from operator import attrgetter
 
 
 def format_str_for_namedtuple(stack):
+    """
+    A helper function that takes a list of strings and replaces
+    the spaces of the strings with '_' so that is will be easy
+    to create a namedtuple.
+
+    :param stack: list of strings
+    :return: list of strings
+    """
     result = []
     for item in stack:
         tempt = item.replace(" ", "_")
@@ -12,8 +20,9 @@ def format_str_for_namedtuple(stack):
     return result
 
 
-def get_file_info(file):
+def read_csv(file):
     """
+    Reads the CSV file and creates a list of named tuples.
 
     :param file: str - Path the csv file
     :return: list of data, boolean - denotes if it has a header,
@@ -63,10 +72,16 @@ def get_file_info(file):
         return data_list, header, dialect
 
 
-def is_number(num):
+def is_number(data):
+    """
+    Checks to see if I can passed parameter can be converted to
+    an int
+    :param data: string (typically)
+    :return: Boolean
+    """
     result = False
     try:
-        int(num.Number_of_Test_Takers)
+        int(data.Number_of_Test_Takers)
         result = True
     except ValueError:
         pass
@@ -74,6 +89,14 @@ def is_number(num):
 
 
 def clean_data(stack, fields):
+    """
+    Creates a new list of namedtuples where the number values such as
+    number of test takers, math mean, etc are integers.
+
+    :param stack: list of namedtuples
+    :param fields: list of namedtuple fields
+    :return: new list of namedtuples
+    """
     results = []
     Data = namedtuple("Data", format_str_for_namedtuple(fields))
     for item in stack:
@@ -83,18 +106,15 @@ def clean_data(stack, fields):
     return results
 
 
-def practice(file, named_tuple=None, has_header=False):
-    with open(file, "r") as file:
-        if not named_tuple and has_header:
-            Data = named_tuple("Data", file.readline())
-            print("Data:", Data)
-
-
-def practice2(file):
-    return csv.get_dialect(file)
-
-
 def preview_list(stack, limit):
+    """
+    This functions allows you to preview an iterable by
+    printing out "x" amount of items from it.
+    
+    :param stack: iterable
+    :param limit: int
+    :return:None
+    """
     for index, item in enumerate(stack):
         print(item)
         if index > limit:
@@ -102,23 +122,15 @@ def preview_list(stack, limit):
 
 
 if __name__ == "__main__":
-    stack, has_header, dialect = get_file_info(SAT_CSV)
+    stack, has_header, dialect = read_csv(SAT_CSV)
     print("has_header: ", has_header)
     print("deliminator: ", dialect.delimiter)
     print("has_header:", has_header)
 
-    test = stack[0]
-    print(dir(test))
-    print(test._fields)
-    """
-    Create a dictionary that maps the ... Think about it more...
-    """
-    sorted_list1 = sorted(stack, key=attrgetter("Number_of_Test_Takers"), reverse=False)
+    filtered_data = filter(is_number, stack)
+    new_data = clean_data(filtered_data, stack[0]._fields)
 
-    sorted_list2 = filter(is_number, stack)
-    sorted_list2 = clean_data(sorted_list2, test._fields)
-    sorted_list2 = sorted(sorted_list2, key=attrgetter("Number_of_Test_Takers"), reverse=False)
+    new_data = sorted(new_data, key=attrgetter("Mathematics_Mean"), reverse=True)
 
-    preview_list(sorted_list1, 10)
     print("\n\n\n")
-    preview_list(sorted_list2, 10)
+    preview_list(new_data, 10)
